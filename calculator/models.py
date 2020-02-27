fd88ae5b25dc7751ad53c6e768189ee6f9ad4727
Vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-02-27 15:33:18 +0700

    Add models to save statement

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Operation(models.Model):
+    x = models.FloatField(default=0)
+    y = models.FloatField(default=0)
+    op = models.CharField(max_length=30)
