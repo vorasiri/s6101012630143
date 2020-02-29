aa8ff70c0e471548522da6829a54da2613223213
Vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-02-29 19:46:42 +0700

    Edit FT to check result

--- functional_tests.py
+++ functional_tests.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
+browser = webdriver.Firefox()
 def test_user_can_checkout_homepage(self):
-    browser = webdriver.Firefox()
     browser.get('http://localhost:8000')
+    assert 'HOME' in browser.title
     time.sleep(30)
-
+    self.assertIn('Calculator POST method', [link.text for link in main.find_elements_by_tag_name('a')])
+    self.assertIn('Calculator GET method', [link.text for link in main.find_elements_by_tag_name('a')])
+    self.assertIn('About', [link.text for link in main.find_elements_by_tag_name('a')])
 
 def test_user_can_calculate_numbers(self):
-    browser = webdriver.Firefox()
+    
     browser.get('http://localhost:8000')
     time.sleep(30)
+    self.assertIn('Calculator POST method', [link.text for link in main.find_elements_by_tag_name('a')])
+    
     # Found x and y field
     variableX = self.browser.find_element_by_id('x')
     variableY = self.browser.find_element_by_id('y')
@@ -24,9 +30,13 @@ def test_user_can_calculate_numbers(self):
     button_divide = self.browser.find_element_by_id('op_divide')
     self.assertTrue(button_divide)
     # Type value x=150.23, y=90 and click minus button
-    variableX.send_keys('150.23')
+    variableX.send_keys('150.2')
     time.sleep(10)
-    variableX.send_keys('90')
+    variableY.send_keys('90')
     time.sleep(10)
     button_minus.send_keys(Keys.ENTER)
+    time.sleep(30)
+    # Check the result
+    self.assertIn('Result:', [link.text for link in main.find_elements_by_tag_name('span')])
+    self.assertIn('60.2', [link.text for link in main.find_elements_by_tag_name('span')])
     time.sleep(30)
